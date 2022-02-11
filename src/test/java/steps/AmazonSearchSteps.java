9fdb0889dba073634334e503cba61e0653cf6f77
camilo <cposadaa@gmail.com>
2022-02-10 21:46:10 -0500

    Add configuration for log4j2 add 2 Appenders (Console and File) the log file will be located in the target folder classes/logs/test.log

--- src/test/java/steps/AmazonSearchSteps.java
+++ src/test/java/steps/AmazonSearchSteps.java
@@ -4,30 +4,35 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AmazonSearchSteps {
+
+    private static final Logger LOGGER = LogManager.getLogger(AmazonSearchSteps.class);
+
     @Given("the user navigates to {string}")
-    public void theUserNavigatesTo(String arg0) {
-        System.out.println("step 1");
+    public void theUserNavigatesTo(String url) {
+        LOGGER.info("step 1");
     }
 
     @When("the user searches for {string}")
-    public void theUserSearchesFor(String arg0) {
-        System.out.println("step 1");
+    public void theUserSearchesFor(String search) {
+        LOGGER.info("step 1");
     }
 
     @And("navigates to the second page")
     public void navigatesToTheSecondPage() {
-        System.out.println("step 1");
+        LOGGER.info("step 1");
     }
 
     @And("selects the third item")
     public void selectsTheThirdItem() {
-        System.out.println("step 1");
+        LOGGER.info("step 1");
     }
 
     @Then("the user is able to add the item to the cart")
     public void theUserIsAbleToAddTheItemToTheCart() {
-        System.out.println("step 1");
+        LOGGER.info("step 1");
     }
 }
