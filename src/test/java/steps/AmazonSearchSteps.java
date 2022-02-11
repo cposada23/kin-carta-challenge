14a7958fe6b0446cc240081865889e57d2ced1c6
camilo <cposadaa@gmail.com>
2022-02-10 21:54:22 -0500

    Read test.properties in the setUp method @Before

--- src/test/java/steps/AmazonSearchSteps.java
+++ src/test/java/steps/AmazonSearchSteps.java
@@ -1,15 +1,38 @@
 package steps;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class AmazonSearchSteps {
 
     private static final Logger LOGGER = LogManager.getLogger(AmazonSearchSteps.class);
+
+    @Before()
+    public void setUp() {
+        // Browser setup, property read from the test.properties file
+
+        try (
+                InputStream input = new FileInputStream("src/test/resources/config/test.properties")
+        ) {
+            Properties properties = new Properties();
+            properties.load(input);
+            LOGGER.info("Executing test in Browser: " + properties.getProperty("browser"));
+            LOGGER.info("Headless Mode: " + properties.get("headless"));
+        } catch (IOException ex) {
+            Assertions.fail("could not load the configuration file"+ ex.getMessage());
+        }
+    }
 
     @Given("the user navigates to {string}")
     public void theUserNavigatesTo(String url) {
